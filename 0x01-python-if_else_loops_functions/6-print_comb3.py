ef1112c6d6847b7d8c4945777b3622d0afd0567c
tropicalmonroe <shitsamajohn821@gmail.com>
2022-06-28 05:29:03 -0700

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for x in range(10):
     for y in range(10):
         if x < y:
             print("{:d}{:d}".format(x, y), end="")
-            if x <= 8:
-                print (", ", end="")
+            if x < 8:
+                print(", ", end="")
 print()
