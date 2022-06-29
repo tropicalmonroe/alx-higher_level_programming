08ddf345c842c04d037b3c8586703070b3038216
tropicalmonroe <shitsamajohn821@gmail.com>
2022-06-29 00:56:53 -0700

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    juu = 0
-    for x in str:
-        if ord(x) >= ord('a') and ord(x) <= ord('z'):
-            juu = 32
-        else:
-            juu = 0
-            print("{:s}".format(ord(x) - juu), end="")
-    print()
+        """Print a string in uppercase."""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 123:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
