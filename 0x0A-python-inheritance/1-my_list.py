8ac5c3173256fc0c4d179534477fd5e9e8c1189c
tropicalmonroe <shitsamajohn821@gmail.com>
2022-08-01 11:11:07 -0700

    Mandatory task 0 to 2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a class MyList that inherits from list"""
+
+class MyList(list):
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
+    def print_sorted(self):
+        """prints the sorted list"""
+        print(sorted(self))
