b42e2f14c4d9d402b7613e03bc7ebbd77d90ab58
tropicalmonroe <shitsamajohn821@gmail.com>
2022-09-06 13:43:13 -0700

    Advanced task

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- Displiays the 3 cities with the highest average temperature between July n Aug
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
