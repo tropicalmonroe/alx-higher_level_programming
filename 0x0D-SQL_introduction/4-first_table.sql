ba3096375b1e1008af9237596f9fb7974e66782d
tropicalmonroe <shitsamajohn821@gmail.com>
2022-09-06 12:52:07 -0700

    Mandatory task fix

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- a script that creates a table in the current database in your MySQL server
-CREATE TABLES IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
