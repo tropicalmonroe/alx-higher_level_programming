771a56b98e65ba9dc56a91b52b056378c2e28d20
tropicalmonroe <shitsamajohn821@gmail.com>
2022-09-07 05:48:15 -0700

    Mandatory task

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 --  a script that creates the MySQL server user
-CREATE USER IF NOT EXIST user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost
+CREATE USER IF NOT EXIST 'user_0d_1@localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1@localhost'
