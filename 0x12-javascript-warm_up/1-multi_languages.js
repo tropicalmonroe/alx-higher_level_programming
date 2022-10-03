e405215a072955805e96366ed8c2e3c24667c9f0
tropicalmonroe <shitsamajohn821@gmail.com>
2022-10-03 07:24:54 -0700

    Task complete fix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const myvar1 = "C is fun";
-const myvar2 = "Python is cool";
-const myvar3 = "JavaScript is amazing";
+const myvar1 = 'C is fun';
+const myvar2 = 'Python is cool';
+const myvar3 = 'JavaScript is amazing';
 console.log(myvar1);
 console.log(myvar2);
 console.log(myvar3);
