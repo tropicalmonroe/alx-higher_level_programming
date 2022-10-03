ed8857512a4f662a07d6650ab7e17917dbdeb1ef
tropicalmonroe <shitsamajohn821@gmail.com>
2022-10-03 07:20:37 -0700

    Task complete

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const myvar1 = "C is fun";
+const myvar2 = "Python is cool";
+const myvar3 = "JavaScript is amazing";
+
+console.log(myvar1);
+console.log(myvar2);
+console.log(myvar3);
