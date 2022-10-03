078fbd45e3c9f52f518d8013266f660c44402369
tropicalmonroe <shitsamajohn821@gmail.com>
2022-10-03 08:13:09 -0700

    task complete fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const string = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let x = 0; x < sting.length; x++) {
+for (let x = 0; x < string.length; x++) {
   console.log(string[x]);
 }
