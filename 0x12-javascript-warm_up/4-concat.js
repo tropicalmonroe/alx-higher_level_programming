ec990c49227fed74a3a3ee3dfa817e0ce6b96bdf
tropicalmonroe <shitsamajohn821@gmail.com>
2022-10-03 08:04:36 -0700

    task complete

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,2 @@
+#!/usr/bin/node
+connsole.log(process.argv[2] + 'is' + process.argv[3]);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const string = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let x = 0; x < sting.length; x++) {
+  console.log(string[x]);
+}
