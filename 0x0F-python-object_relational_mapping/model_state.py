7b453ae6c7b5e6d68be049089d191f2e74debd3e
tropicalmonroe <shitsamajohn821@gmail.com>
2022-10-08 04:26:07 -0700

    Task complete fix

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""Start link class to table in database """
-import sys
-from model_state import Base, State
+"""
+First state model
+Definition of the State class
+Models State inherits from base
+"""
 
-from sqlalchemy import (create_engine)
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1],
-                                   sys.argv[2],
-                                   sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+Base = declarative_base()
+
+
+class State(Base):
+    """ Class definition of a State and an instance
+    Base = declarative_base() """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
