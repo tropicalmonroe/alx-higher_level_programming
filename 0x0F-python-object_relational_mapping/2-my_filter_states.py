421d21cde432719499f74990f3c37c087844b5f0
tropicalmonroe <shitsamajohn821@gmail.com>
2022-10-08 03:44:06 -0700

    Task complete fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" Displays all values in the states table of hbtn_0e_0_usa where name matches the argument
+""" This script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument.
 """
-
 if __name__ == '__main__':
     import MySQLdb
     import sys
