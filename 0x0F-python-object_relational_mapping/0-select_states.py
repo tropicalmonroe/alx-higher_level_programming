43544a21110c1e7285f7d6ec25fc72ec4ecd8d3b
tropicalmonroe <shitsamajohn821@gmail.com>
2022-10-08 03:03:38 -0700

    Task complete fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3])
-    cur = conn.cursor()
+    cur = db.cursor()
     cur.execute('SELECT * FROM states ORDER BY id ASC')
     rows = cur.fetchall()
     for row in rows:
